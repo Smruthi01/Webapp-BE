0cb057580cbd5e19d5e63edeedb1f6ca5b4a5ed3
Smruthi <68817254+Smruthi01@users.noreply.github.com>
2022-12-01 14:43:26 +0530

    Update Webappconfig.java
    added cf link

--- src/main/java/com/webapp/webapp/config/Webappconfig.java
+++ src/main/java/com/webapp/webapp/config/Webappconfig.java
@@ -10,8 +10,10 @@ public class Webappconfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET","POST","PUT","DELETE");
+                 .allowedOrigins("https://webappelevate.devops.coda.run/")        
+                 .allowedMethods("GET","POST","PUT","DELETE");
     }
     
 }
+
+
