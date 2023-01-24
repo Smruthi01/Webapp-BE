638257ded497da3f9e8e4b5fc30d5745eb73259c
Smruthi <68817254+Smruthi01@users.noreply.github.com>
2023-01-24 16:30:43 +0530

    Update Dbconfig.java

--- src/main/java/com/webapp/webapp/config/Dbconfig.java
+++ src/main/java/com/webapp/webapp/config/Dbconfig.java
@@ -1,42 +1,42 @@
-package com.webapp.webapp.config;
+// package com.webapp.webapp.config;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import javax.sql.DataSource;
+// import com.fasterxml.jackson.databind.JsonNode;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.boot.jdbc.DataSourceBuilder;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import javax.sql.DataSource;
 
 
 
-@Configuration
-public class Dbconfig {
+// @Configuration
+// public class Dbconfig {
     
-    @Value("${spring.datasource.url}")
-    private String url;
+//     @Value("${spring.datasource.url}")
+//     private String url;
 
-    @Autowired
-    private SecretManager secretManager;
+//     @Autowired
+//     private SecretManager secretManager;
 
-    @Bean
+//     @Bean
 
-    public DataSource dataSource() {
+//     public DataSource dataSource() {
 
 
 
-        JsonNode secret = secretManager.getSecret();
+//         JsonNode secret = secretManager.getSecret();
 
-        if (secret == null)
+//         if (secret == null)
 
-            return null;
+//             return null;
 
-        return DataSourceBuilder.create()
-                // .driverClassName(driver)
-                .url(url)
-                .username(secret.get("username").asText())
-                .password(secret.get("password").asText())
-                .build();
+//         return DataSourceBuilder.create()
+//                 // .driverClassName(driver)
+//                 .url(url)
+//                 .username(secret.get("username").asText())
+//                 .password(secret.get("password").asText())
+//                 .build();
 
-    }
-}
+//     }
+// }
